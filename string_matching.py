5c2c526658f8243e582319c6e6717bc86f85340b
Souradeep Ghosh <53990920+Sourolio10@users.noreply.github.com>
2022-11-04 12:19:54 -0400

    Add files via upload

--- string_matching.py
+++ string_matching.py
@@ -6,14 +6,42 @@ import os
 import json
 import math
 from openpyxl import load_workbook
-notebook_path = os.path.abspath("string_matching.py")
-uber = os.path.join(os.path.dirname(notebook_path), "uber2.csv")
-ubergraph_response_df = pd.read_csv(uber, encoding = "ISO-8859-1", header =0)
+import rdflib
+import itertools
+g = rdflib.Graph()
+#g.parse("https://ubergraph.apps.renci.org/sparql")
+#print(len(g))
+
+knows_query = """
+
+PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+PREFIX owl: <http://www.w3.org/2002/07/owl#>
+PREFIX overlaps: <http://purl.obolibrary.org/obo/RO_0002131>
+PREFIX cell: <http://purl.obolibrary.org/obo/CL_0000000>
+PREFIX description: <http://www.w3.org/2000/01/rdf-schema#comment>
+PREFIX definition: <http://purl.obolibrary.org/obo/IAO_0000115>
+PREFIX synonym: <http://www.geneontology.org/formats/oboInOwl#hasExactSynonym>
+SELECT DISTINCT ?cell ?cell_label ?cell_description ?cell_definition ?parent_cell ?parent_label ?synonym WHERE {
+    SERVICE <https://ubergraph.apps.renci.org/sparql>{
+    ?cell rdfs:label ?cell_label .
+    ?cell rdfs:subClassOf cell: .
+    ?cell rdfs:subClassOf ?parent_cell .
+    ?cell description: ?cell_description .
+    ?cell definition: ?cell_definition .
+    ?parent_cell rdfs:subClassOf cell: .
+    ?parent_cell rdfs:label ?parent_label .
+    ?cell synonym: ?synonym .
+}}"""
+qres = g.query(knows_query)
+print(qres) #checking query returned object
+import pandas as pd
+b = pd.DataFrame(qres, columns= qres.vars) #converting it into dataframe
+c=0
 import re
 cellLabel = 'MC(T)'
 cellLabel1 = re.sub(r"[^a-zA-Z0-9 ]","",cellLabel) #removing spaces and special characters
 c=0
-for row in ubergraph_response_df.iterrows():
+for row in b.iterrows():
     rs= re.sub(r"[^a-zA-Z0-9 ]","",row[1][1])
     rsy = re.sub(r"[^a-zA-Z0-9 ]","",row[1][6])
     if cellLabel.strip().lower().replace(" ", "") == row[1][1].strip().lower().replace(" ", ""):
